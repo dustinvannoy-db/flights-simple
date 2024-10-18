4edd010385d9108f3f139ef946146ab591b0a240
dustinvannoy-db <dustin.vannoy@databricks.com>
2024-10-18 16:03:05 -0700

    Try different databricks-connect version

--- tests/unit_transforms/test_flight_transforms.py
+++ tests/unit_transforms/test_flight_transforms.py
@@ -19,9 +19,9 @@ def spark_session():
     try:
         from databricks.connect import DatabricksSession
         return DatabricksSession.builder.getOrCreate()   
-    except (ValueError, RuntimeError):
-        from databricks.connect import DatabricksSession
-        return DatabricksSession.builder.profile("unit_tests").getOrCreate()    
+    # except (ValueError, RuntimeError):
+    #     from databricks.connect import DatabricksSession
+    #     return DatabricksSession.builder.profile("unit_tests").getOrCreate()    
     except ImportError:
         print("No Databricks Connect, build and return local SparkSession")
         from pyspark.sql import SparkSession
