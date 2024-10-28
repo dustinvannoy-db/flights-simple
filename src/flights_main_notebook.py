189c8cfe7777c8200cf8833e6eb8c042b5c155f2
dustinvannoy-db <dustin.vannoy@databricks.com>
2024-10-27 21:53:43 -0700

    Restructure to support serverless or classic job compute

--- src/flights_main_notebook.py
+++ src/flights_main_notebook.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 dbutils.widgets.text("catalog", "main")
-dbutils.widgets.text("database", "dustinvannoy_dev")
+dbutils.widgets.text("database", "flights_dev")
 # artifact_path = f'/Workspace{dbutils.widgets.get("artifact_path")}/.internal'
 
 # COMMAND ----------
@@ -19,8 +19,6 @@ dbutils.widgets.text("database", "dustinvannoy_dev")
 from flights.transforms import flight_transforms, shared_transforms
 from flights.utils import flight_utils
 
-from flights.utils import flight_utils
-
 catalog = dbutils.widgets.get("catalog")
 database = dbutils.widgets.get("database")
 
@@ -32,19 +30,7 @@ raw_table_name = f"{catalog}.{database}.flights_raw"
 
 # DBTITLE 1,Read raw
 df = flight_utils.read_batch(spark, path).limit(1000)
-df = flight_utils.read_batch(spark, path).limit(1000)
 display(df)
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ## Transform data
-
-# COMMAND ----------
-df_transformed = (
-        df.transform(flight_transforms.delay_type_transform)
-          .transform(shared_transforms.add_metadata_columns)
-    )
 
 # COMMAND ----------
 
@@ -64,6 +50,5 @@ df_transformed = (
 
 # COMMAND ----------
 
-df_transformed.write.format("delta").mode("append").saveAsTable(raw_table_name)
 df_transformed.write.format("delta").mode("append").saveAsTable(raw_table_name)
 print(f"Succesfully wrote data to {raw_table_name}")
